3cbe5c1f59bab0428931bbbf2c00b499880d5a71
Matheus Henrique <Matheushrnque@gmail.com>
2025-11-19 00:50:27 -0300

    teste de login AINDA NÃO COMPLETO

---
+++ view_calazzans/js/testelogin.js
@@ -0,0 +1,41 @@
+const form = document.getElementById('login');
+
+        form.addEventListener('submit', async (event) => {
+            event.preventDefault(); // Evita que a página recarregue
+
+            const email = document.getElementById('email').value;
+            const senha = document.getElementById('senha').value;
+
+            //Monta o objeto do DTO LoginRequest 
+            const dadosLogin = {
+                email: email,
+                senha: senha
+            };
+
+            try {
+                const response = await fetch('http://localhost:8080/auth/login', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(dadosLogin)
+                });
+
+                if (response.ok) {
+                    const usuario = await response.json();
+                    alert("Login realizado com sucesso! Bem-vindo " + usuario.email);
+
+                    setTimeout(() => {
+                window.location.href = "../view/GerenciarCalazzans.html"
+            }, 1000)
+
+                } else {
+                    const erroTexto = await response.text();
+                    document.getElementById('mensagem').innerText = erroTexto
+                }
+
+            } catch (error) {
+                console.error('Erro:', error);
+                document.getElementById('mensagem').innerText = "Erro ao conectar com o servidor."
+            }
+        })
